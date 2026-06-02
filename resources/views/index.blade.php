a63d378bb27f17fd61613f3d0ef8ec7b4cc8db0f
saka-C <shaka.aufa.alhajar@gmail.com>
2026-06-02 20:44:16 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+
+    @vite(['resources/css/app.css'])
+
+</head>
+
+<body class=" overflow-x-hidden">
+
+    <body>
+        <div class="flex flex-col w-full items-start px-10 py-4 relative bg-[#f9f9f9]">
+            <div class="flex w-full items-center justify-between px-6 py-0 relative w-full flex-[0_0_auto]">
+                <div class="inline-flex items-center gap-2 relative flex-[0_0_auto]">
+                    <div class="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                        <div
+                            class="relative flex items-center w-fit mt-[-1.00px] [font-family:'Montserrat-Regular',Helvetica] font-normal text-[#ba000c] text-5xl tracking-[-0.96px] leading-[52.8px] whitespace-nowrap">
+                            GEN</div>
+                    </div>
+                    <div class="flex flex-col w-[17px] h-8 items-start px-2 py-0 relative">
+                        <div class="relative w-px h-8 bg-[#e7bdb7]"></div>
+                    </div>
+                    <div class="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                        <div
+                            class="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#5f5e5e] text-[10px] tracking-[1.00px] leading-[12.5px]">
+                            GERAKAN<br />EKSPLORASI<br />NUSANTARA</div>
+                    </div>
+                </div>
+                <div class="flex w-[405.65px] items-center justify-center gap-8 relative">
+                    <div class="relative flex-1 grow h-[16.8px]">
+                        <div
+                            class="absolute -top-px left-0 h-[17px] flex items-center [font-family:'Inter-Regular',Helvetica] font-normal text-[#5f5e5e] text-sm tracking-[0.70px] leading-[16.8px] whitespace-nowrap">
+                            Tentang</div>
+                    </div>
+                    <div class="relative w-[63px] h-[17px]">
+                        <div
+                            class="absolute -top-px left-0 h-[17px] flex items-center [font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#5f5e5e] text-sm tracking-[0.70px] leading-[16.8px] whitespace-nowrap">
+                            Program</div>
+                    </div>
+                    <div class="relative w-[51px] h-[17px]">
+                        <div
+                            class="absolute -top-px left-0 h-[17px] flex items-center [font-family:'Inter-Regular',Helvetica] font-normal text-[#5f5e5e] text-sm tracking-[0.70px] leading-[16.8px] whitespace-nowrap">
+                            Donasi</div>
+                    </div>
+                    <div class="relative w-[49px] h-[17px]">
+                        <div
+                            class="absolute -top-px left-0 h-[17px] flex items-center [font-family:'Inter-Regular',Helvetica] font-normal text-[#5f5e5e] text-sm tracking-[0.70px] leading-[16.8px] whitespace-nowrap">
+                            Artikel</div>
+                    </div>
+                    <div class="relative flex-1 grow h-[16.8px]">
+                        <div
+                            class="absolute -top-px left-0 h-[17px] flex items-center [font-family:'Inter-Regular',Helvetica] font-normal text-[#5f5e5e] text-sm tracking-[0.70px] leading-[16.8px] whitespace-nowrap">
+                            Kontak</div>
+                    </div>
+                </div>
+                <button
+                    class="all-[unset] box-border inline-flex flex-col items-center justify-center px-8 py-3 relative flex-[0_0_auto] bg-[#e31d1d] rounded-full">
+                    <div
+                        class="absolute w-full h-full top-0 left-0 bg-[#ffffff01] rounded-full shadow-[0px_2px_4px_-2px_#0000001a,0px_4px_6px_-1px_#0000001a]">
+                    </div>
+                    <div
+                        class="relative flex items-center justify-center w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-sm text-center tracking-[0.70px] leading-[16.8px] whitespace-nowrap">
+                        Bergabung</div>
+                </button>
+            </div>
+        </div>
+    </body>
+
+</body>
+
+</html>
